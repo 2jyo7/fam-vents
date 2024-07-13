99e160936ae49099ecfd5c6d32021b03839c10a5
2jyo7 <jyoti.kri2697@gmail.com>
2024-07-13 14:25:58 +0530

    second commit

--- index.js
+++ index.js
@@ -113,28 +113,53 @@ function scrollToServices() {
     }
 }
 
-function scrollToItems1() {
-    let items1 =  document.getElementById("items1");
-    if (items1) {
-        items1.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-    let items2 =  document.getElementById("items2");
-    if (items2) {
-        items2.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-    let items3 =  document.getElementById("items3");
-    if (items3) {
-        items3.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-    let items4 =  document.getElementById("items4");
-    if (items4) {
-        items4.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-}
 
 
 document.getElementById("btnTab").addEventListener("click", function(e) {
     document.getElementById("ventLists").style.display = "block";
 })
 
+//paragraphe truncate 
+
+function gallery(){
+    this.index=0;
+    this.load=function(){
+      this.rootEl = document.querySelector(".gallery");
+      this.platform = this.rootEl.querySelector(".platform");
+      this.frames = this.platform.querySelectorAll(".each-frame");
+      this.contentArea = this.rootEl.querySelector(".content-area");      
+      this.width = parseInt(this.rootEl.style.width);
+      this.limit = {start:0,end:this.frames.length-1};
+      this.frames.forEach(each=>{each.style.width=this.width+"px";});   
+      this.goto(this.index);      
+    }
+    this.set_title = function(){this.rootEl.querySelector(".heading").innerText=this.frames[this.index].getAttribute("title");}
+    this.next = function(){this.platform.style.right=this.width * ++this.index + "px";this.set_title();}
+    this.prev = function(){this.platform.style.right=this.width * --this.index + "px";this.set_title();}
+    this.goto = function(index){this.platform.style.right = this.width * index + "px";this.index=index;this.set_title();}
+    this.load();
+}
+var G = new gallery();
+  G.rootEl.addEventListener("click",function(t){
+      var val = t.target.getAttribute("action");
+      if(val == "next" && G.index != G.limit.end){G.next();}
+      if(val == "prev" && G.index != G.limit.start){G.prev();}
+      if(val == "goto"){
+          let rv = t.target.getAttribute("goto");
+          rv = rv == "end" ? G.limit.end:rv;
+          G.goto(parseInt(rv));
+      }
+  });
+  document.addEventListener("keyup",function(t){
+      var val = t.keyCode;
+      if(val == 39 && G.index != G.limit.end){G.next();}
+      if(val == 37 && G.index != G.limit.start){G.prev();}
+    });
+
+  // run G.load() if new data loaded with ajax
+    
+  function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('hidden');
+  }
 
